4f5ca87e5ca42f21b199b593d7df9b9597474068
Juswa <juswajuswagono@gmail.com>
2024-09-12 07:38:03 +0800

    setting up the sign in/ sign up controllers and routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,57 @@
-import User from "../models/user.model.js"
+import { handleMakeError } from "../middleware/handleError.js";
+import User from "../models/user.model.js";
 
 export const signup = async (req, res, next) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+  const { email, username, password, confirmPassword } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) return next(handleMakeError(400, "User already exist"));
+
+  if (password !== confirmPassword)
+    return next(handleMakeError(400, "both passwords should be equal"));
+
+  try {
+    const newUser = new User({
+      email,
+      username,
+      password,
+    });
+
+    await newUser.save();
+    res.status(201).json({
+      _id: newUser._id,
+      email: newUser.email,
+      username: newUser.username,
+      role: newUser.role,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
     try {
-        
+      const validUser = await User.findOne({ email });
+      if (!validUser) return next(handleMakeError(404, "No user found!"));
+  
+      if (validUser && (await validUser.comparePassword(password))) {
+        // const { accessToken, refreshToken } = generateTokens(validUser._id);
+        // await storeRefreshToken(validUser._id, refreshToken);
+        // setCookies(res, accessToken, refreshToken);
+  
+        // EXCLUDING THE PASSWORD WITH THIS METHOD INSTEAD OF .select("-password") is wild
+        // JOKES ON YOU I CANT USE .select("-password") in this messy code because if i put that after User.FindOne - 
+        // now i cant compare my password because it wouldnt work because there is no password to compare
+        const { password: pass, ...rest } = validUser._doc;
+        res.json(rest);
+      } else {
+        next(handleMakeError(400, "Invalid Credentials"));
+      }
     } catch (error) {
-        
+      next(error);
+      console.log("Error in sign-in controller");
     }
-}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+import cookieParser from 'cookie-parser'
+// routes
+import authRoutes from '../backend/routes/auth.route.js'
+import { connectDb } from './db/db.js'
+import { handleError } from './middleware/handleError.js'
+
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT 
+app.use(express.json())
+app.use(cookieParser())
+app.use(handleError)
 
-app.use('/api/v1/auth', authRoutes)
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => {
+    connectDb()
     console.log("Server running on this " + PORT)
 })
